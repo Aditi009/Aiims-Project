c7ddfe24ce0edecbb1854a6606b398ea170482f5
Aditi Gupta <aditissdn@gmail.com>
2022-12-07 11:15:42 +0530

    commit

--- resources/views/web/commitee.blade.php
+++ resources/views/web/commitee.blade.php
@@ -60,83 +60,83 @@
     </style>
     <div class="about-area">
         <div class="container">
-            <div class="row align-items-center" style="display: flex;justify-content: center;">
+           
+            <div class="row align-items-center mobile-center">
                 <div class="com-card">
                     <img src="{{ asset('web/images/deeparora.png') }}">
-                    <p class="body-text">Patron
+                    <p class="body-text">Prof. Lokesh Kashyap
                     </p>
-                    <p class="body-bottom">Dr Randeep Guleria </p>
-                    <p class="body-bottom">Director , AIIMS </p>
+                    <p class="body-bottom">Head of Department </p>
+                    <p class="body-bottom">Anaesthesiology, Pain Medicine and Critical Care
+                        AIIMS, New Delhi </p>
+
+
+                </div>
+                <div class="com-card">
+                    <img src="{{ asset('web/images/deeparora.png') }}">
+                    <p class="body-text">Prof. Anjolie Chhabra
+                    </p>
+                    <p class="body-bottom"> </p>
+                    <p class="body-bottom"></p>
 
 
                 </div>
             </div>
 
-
-            <div class="row align-items-center mobile-center">
+            <div class="row align-items-center mobile-center mt-2">
                 <div class="com-card">
                     <img src="{{ asset('web/images/deeparora.png') }}">
-                    <p class="body-text">Patron
+                    <p class="body-text">Dr Bikash Ranjan Ray
                     </p>
-                    <p class="body-bottom">Dr Randeep Guleria </p>
-                    <p class="body-bottom">Director , AIIMS </p>
+                    <p class="body-bottom"></p>
+                    <p class="body-bottom"></p>
 
 
                 </div>
                 <div class="com-card">
                     <img src="{{ asset('web/images/deeparora.png') }}">
-                    <p class="body-text">Patron
+                    <p class="body-text">Dr. Debesh Bhoi
                     </p>
-                    <p class="body-bottom">Dr Randeep Guleria </p>
-                    <p class="body-bottom">Director , AIIMS </p>
-
-
-                </div>
-            </div>
-
-            <div class="row align-items-center mobile-center">
-                <div class="com-card">
-                    <img src="{{ asset('web/images/deeparora.png') }}">
-                    <p class="body-text">Patron
-                    </p>
-                    <p class="body-bottom">Dr Randeep Guleria </p>
-                    <p class="body-bottom">Director , AIIMS </p>
-
-
-                </div>
-                <div class="com-card">
-                    <img src="{{ asset('web/images/deeparora.png') }}">
-                    <p class="body-text">Patron
-                    </p>
-                    <p class="body-bottom">Dr Randeep Guleria </p>
-                    <p class="body-bottom">Director , AIIMS </p>
+                    <p class="body-bottom"></p>
+                    <p class="body-bottom"></p>
 
 
                 </div>
             </div>
 
+         
+
             <div class="row align-items-center mobile-center">
                 <div class="com-card">
                     <img src="{{ asset('web/images/deeparora.png') }}">
-                    <p class="body-text">Patron
+                    <p class="body-text">Dr. Heena Garg
                     </p>
-                    <p class="body-bottom">Dr Randeep Guleria </p>
-                    <p class="body-bottom">Director , AIIMS </p>
+                    <p class="body-bottom"></p>
+                    <p class="body-bottom"> </p>
 
 
                 </div>
                 <div class="com-card">
                     <img src="{{ asset('web/images/deeparora.png') }}">
-                    <p class="body-text">Patron
+                    <p class="body-text">Dr. Amit Kumar
                     </p>
-                    <p class="body-bottom">Dr Randeep Guleria </p>
-                    <p class="body-bottom">Director , AIIMS </p>
+                    <p class="body-bottom"> </p>
+                    <p class="body-bottom"></p>
 
 
                 </div>
                 <div class="com-card">
                     <img src="{{ asset('web/images/deeparora.png') }}">
-                    <p class="body-text">Patron
+                    <p class="body-text">Dr. Neha Pangasa
+                    </p>
+                    <p class="body-bottom"> </p>
+                    <p class="body-bottom"> </p>
+
+
+                </div>
+                <div class="com-card">
+                    <img src="{{ asset('web/images/deeparora.png') }}">
+                    <p class="body-text">Dr. Sachin Kumar
                     </p>
                     <p class="body-bottom">Dr Randeep Guleria </p>
                     <p class="body-bottom">Director , AIIMS </p>
