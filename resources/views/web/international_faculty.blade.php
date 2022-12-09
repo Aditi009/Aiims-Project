f57a1345fdace712eeb23dd045717f1ab752b59b
Aditi Gupta <aditissdn@gmail.com>
2022-12-09 22:23:35 +0530

    commit

--- resources/views/web/international_faculty.blade.php
+++ resources/views/web/international_faculty.blade.php
@@ -9,9 +9,177 @@
             </ul>
         </div>
     </div> --}}
-
+    <style>
+        .body-text {
+            text-align: center;
+            width: max-content;
+            color: red;
+            font-size: 20px;
+            font-weight: 600;
+        }
+        
+        .body-bottom {
+            color: #5698f4;
+            font-weight: 500;
+            font-size: 17px;
+            margin-top: -10px;
+        
+        }
+        
+        @media only screen and (max-width: 600px) {}
+        
+        .about-area {
+            padding-bottom: 60px;
+        }
+        
+        
+        /****y styles */
+        .myprofilecard {
+            border: none;
+            width: auto;
+        }
+        
+        .myprofilecard img {
+            border-radius: 50%;
+            height: 180px;
+            width: 180px;
+            object-fit: cover;
+            border: 4px solid #5698f4;
+        }
+        
+        .topcardrow .myprofilecard {
+            width: 30rem;
+        }
+        
+        .ocheading {
+            color: #7e1916;
+            font-weight: 900;
+        }
+        
+        .organiserslist li {
+            color: #5698f4;
+            margin-left: 4px;
+        }
+        
+        .listcontainer {
+            margin-top: 40px;
+        }
+        </style>
 
     <div class="team-area py-120">
+     
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 mx-auto">
+                    <div class="site-heading text-center">
+                        <span class="site-title-tagline">Team</span>
+                        <h2 class="site-title">Meet With <span>Experts</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center mt-5">
+    
+                <div class="col-md-2 card myprofilecard align-items-center">
+                    <img class="card-img-top"  src="{{asset('web/images/philippe.png')}}" alt="Card image cap">
+                    <div class="card-body">
+                        <div class="card-text text-center">
+                            <p class="body-text">Workshop coordinators
+                            </p>
+                            <p class="body-bottom">Dr. Heena Garg</p>
+                        </div>
+    
+                    </div>
+                </div>
+    
+                <div class="col-md-2 card myprofilecard align-items-center">
+                    <img class="card-img-top" src="{{ asset('web/org/amit.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <div class="card-text text-center">
+                            <p class="body-text">Workshop coordinators
+                            </p>
+                            <p class="body-bottom">Dr. Amit Kumar</p>
+                        </div>
+    
+                    </div>
+                </div>
+                <div class="col-md-2 card myprofilecard align-items-center">
+                    <img class="card-img-top" src="{{ asset('web/org/neha.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <div class="card-text text-center">
+                            <p class="body-text">Workshop coordinators
+                            </p>
+                            <p class="body-bottom">Dr. Neha Pangasa</p>
+                        </div>
+    
+                    </div>
+                </div>
+    
+                <div class="col-md-2 card myprofilecard align-items-center">
+                    <img class="card-img-top" src="{{ asset('web/org/sachine.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <div class="card-text text-center">
+                            <p class="body-text">Workshop coordinators
+                            </p>
+                            <p class="body-bottom">Dr. Sachin Kumar</p>
+                        </div>
+    
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center mt-5">
+    
+                <div class="col-md-2 card myprofilecard align-items-center">
+                    <img class="card-img-top" src="{{ asset('web/org/heenagarg.jpeg') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <div class="card-text text-center">
+                            <p class="body-text">Workshop coordinators
+                            </p>
+                            <p class="body-bottom">Dr. Heena Garg</p>
+                        </div>
+    
+                    </div>
+                </div>
+    
+                <div class="col-md-2 card myprofilecard align-items-center">
+                    <img class="card-img-top" src="{{ asset('web/org/amit.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <div class="card-text text-center">
+                            <p class="body-text">Workshop coordinators
+                            </p>
+                            <p class="body-bottom">Dr. Amit Kumar</p>
+                        </div>
+    
+                    </div>
+                </div>
+                <div class="col-md-2 card myprofilecard align-items-center">
+                    <img class="card-img-top" src="{{ asset('web/org/neha.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <div class="card-text text-center">
+                            <p class="body-text">Workshop coordinators
+                            </p>
+                            <p class="body-bottom">Dr. Neha Pangasa</p>
+                        </div>
+    
+                    </div>
+                </div>
+    
+                <div class="col-md-2 card myprofilecard align-items-center">
+                    <img class="card-img-top" src="{{ asset('web/org/sachine.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <div class="card-text text-center">
+                            <p class="body-text">Workshop coordinators
+                            </p>
+                            <p class="body-bottom">Dr. Sachin Kumar</p>
+                        </div>
+    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    {{-- <div class="team-area py-120">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 mx-auto">
@@ -140,6 +308,6 @@
             </div>
         </div>
     </div>
-    
+     --}}
     @endsection
 
