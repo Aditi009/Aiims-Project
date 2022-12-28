abee8f15bcda0133ffd24079438ebb722548b87f
seraj.alam <seraj.alam@wisdmlabs.com>
2022-12-28 12:25:08 +0530

    speed incresed

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -235,7 +235,15 @@
 
 
     <a href="#" id="scroll-top"><i class="far fa-long-arrow-up"></i></a>
-
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
     <script>
       const nextDate = "24/02/2023"
       const nowDate = moment();
@@ -246,7 +254,7 @@
       $('#day_to').html(html)
   </script>
 
-    
+
     <!-- <script data-cfasync="false" src="{{asset('web/js/cloudflare-static-email-decode.min.js')}}"></script> -->
     <!-- <script src="{{asset('web/js/js-jquery-3.6.0.min.js')}}"></script> -->
     <!-- <script src="{{asset('web/js/js-modernizr.min.js')}}"></script> -->
